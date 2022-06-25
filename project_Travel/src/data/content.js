1f57cb94bfa090fbc4d4ab4897644c99d9f1f34a
Cecilia <qinger.lei0109@gmail.com>
2022-06-25 21:21:12 +0930

    first commit

---
+++ project_Travel/src/data/content.js
@@ -0,0 +1,27 @@
+const CONTENT_INFO = {
+    island:{
+        h1: "Never Stop ",
+        h2: "Explore The World",
+        p: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta, vel fugit! Accusamus labore perspiciatis, voluptate sunt mollitia inventore aut reiciendis est exercitationem quas error atque debitis cum rem harum dolor."
+    },
+    spring:{
+        h1: "Spring",
+        h2: "Feet the new vitality",
+        p: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta, vel fugit! Accusamus labore perspiciatis, voluptate sunt mollitia inventore aut reiciendis est exercitationem quas error atque debitis cum rem harum dolor."
+    },
+    summer:{
+        h1: "Summer",
+        h2: "Enjoy passionate time",
+        p: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta, vel fugit! Accusamus labore perspiciatis, voluptate sunt mollitia inventore aut reiciendis est exercitationem quas error atque debitis cum rem harum dolor."
+    },
+    autumn:{
+        h1: "Autumn",
+        h2: "Experience without limits",
+        p: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta, vel fugit! Accusamus labore perspiciatis, voluptate sunt mollitia inventore aut reiciendis est exercitationem quas error atque debitis cum rem harum dolor."
+    },
+    winter:{
+        h1:"Winter",
+        h2:"Enjoy peace and freedom",
+        p:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta, vel fugit! Accusamus labore perspiciatis, voluptate sunt mollitia inventore aut reiciendis est exercitationem quas error atque debitis cum rem harum dolor."
+    },
+};
